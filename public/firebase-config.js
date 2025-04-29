db6929209075101a95cfbe2d82a244b6bc931750
heelflip91 <heelflip91@gmail.com>
2025-04-29 22:51:07 +0000

    vibes

---
+++ public/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-xGLUiAjUx7PR-UTp2uXgKIT3-YkcOWo",
+  authDomain: "budget-banking-proj.firebaseapp.com",
+  projectId: "budget-banking-proj",
+  storageBucket: "budget-banking-proj.firebasestorage.app",
+  messagingSenderId: "221922223081",
+  appId: "1:221922223081:web:d0ee8cd366e27d601ee178"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
